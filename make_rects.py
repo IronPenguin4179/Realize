c2966754697da9f9ef66751b71290cf89587e595
“[Justin <jrwalker314@yahoo.com>
2019-01-10 03:41:00 -0800

    Begin rectangle drawing classes with tkinter.

---
+++ make_rects.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.geometry("700x500")
+canvas = Canvas(root)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+canvas.grid(column=0, row=0, sticky=(N, W, E, S))
+
+def_x1 = 10
+def_y1 = 10
+def_x2 = 150
+def_y2 = 80
+def_gap = 20
+def add_col():
+    x_1 = x1+x2+gap
+    x_2 = x1+x2
+    print(x1, x2)
+
+    return x_1, y1, x_2, y2
+    
+class Rectangle:
+    def __init__(self):
+        self.x1 = def_x1
+        self.y1 = def_y1
+        self.x2 = def_x2
+        self.y2 = def_y2
+    
+    def create(self):
+        canvas.create_rectangle(self.x1,self.y1,self.x2,self.y2)
+        self.x1 = self.x1+def_x2+def_gap
+        self.x2 = self.x1+(def_x2-def_x1)
+        print(self.x1, self.x2)
+
+rect = Rectangle()
+rect.create()
+rect.create()
+rect.create()
+root.mainloop()
